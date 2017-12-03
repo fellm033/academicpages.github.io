d143a760b696fe5d007d1778ddfa1e69174307d7
fellm033 <31905338+fellm033@users.noreply.github.com>
2017-12-03 16:31:37 -0500

    Update org-locations.js
    Updating brew map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,28 +1,53 @@
 var addressPoints = [
   [
-    "Surly Brewing Company",
+    "1. Surly Brewing Company",
    44.973119,
     -93.209824
   ],
   [
-    "Insight Brewing",
+    "2. Insight Brewing",
     44.991808,
      -93.212973
   ],
   [
-    "Bauhaus Brew Labs",
+    "3. Bauhaus Brew Labs",
     45.000807,
      -93.244726
   ],
   [
-    "Indeed Brewing Company and Taproom",
+    "4. Indeed Brewing Company and Taproom",
     45.003248,
      -93.251612
   ],
   [
-    "New Glarus Brewing Company",
+    "5. New Glarus Brewing Company",
     42.794314,
      -89.630172
+  ],
+  [
+    "6. Ale Asylum",
+    43.120489,
+     -89.354393
+  ],
+  [
+   "7. Popeyes on Lake Geneva",
+   2.590678, 
+    -88.435995
+  ],
+  [
+   "8. The Empirical Taproom"
+   42.008069, 
+    -87.665222
+  ],
+  [
+    "9. Denizens Brewing Co."
+    38.988585, 
+     -77.027243
+  ]
+  [
+    "10. The Big Hunt"
+    38.908457,
+     -77.042436
   ]
   
 ];
